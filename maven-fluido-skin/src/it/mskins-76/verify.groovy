4ee0adfd40c2d9db36cd452439ef61da15af514b
Olivier Lamy <olamy@apache.org>
2013-02-21 21:36:59 +0000

    [MSKINS-76] Add Flattr button integration to Fluido skin Submitted by Michael Koch.
    git-svn-id: https://svn.apache.org/repos/asf/maven/skins/trunk@1448812 13f79535-47bb-0310-9956-ffa450edef68

---
+++ maven-fluido-skin/src/it/mskins-76/verify.groovy
@@ -0,0 +1,18 @@
+File index = new File( basedir, "target/site/index.html" )
+assert index.exists()
+assert 1 == index.getText().count( '''\
+<script type="text/javascript">
+    /* <![CDATA[ */
+        (function() {
+            var s = document.createElement('script'), t = document.getElementsByTagName('script')[0];
+            s.type = 'text/javascript';
+            s.async = true;
+            s.src = 'http://api.flattr.com/js/0.6/load.js?mode=auto';
+            t.parentNode.insertBefore(s, t);
+        })();
+    /* ]]> */</script>''' )
+
+assert 1 == index.getText().count( '''\
+<a class="FlattrButton" style="display:none;"  href="http://opensource.org/"></a>
+    <noscript><a href="http://flattr.com/thing/448050/Open-Source-Initiative-OSI" target="_blank">
+    <img src="http://api.flattr.com/button/flattr-badge-large.png" alt="Flattr this" title="Flattr this" border="0" /></a></noscript>''' )
