7f627b0c40821e49638e54f04c621dd55cc7538c
Michael Osipov <michaelo@apache.org>
2013-07-20 20:04:48 +0000

    Integration tests for - MSKSINS-75 - MSKSINS-76 - MSKSINS-75 topbar
    fail on Windows due to Groovy's GString multiline \n terminator.
    
    Useful hint found on: http://evgeny-goldin.com/blog/groovy-multiline-strings/
    
    git-svn-id: https://svn.apache.org/repos/asf/maven/skins/trunk/maven-fluido-skin@1505195 13f79535-47bb-0310-9956-ffa450edef68

--- src/it/mskins-75/verify.groovy
+++ src/it/mskins-75/verify.groovy
@@ -1,7 +1,7 @@
 File index = new File( basedir, "target/site/index.html" )
 assert index.exists()
 assert 1 == index.getText().count( '''\
-<!-- Piwik -->
+    <!-- Piwik -->
     <script type="text/javascript">
     var pkBaseURL = (("https:" == document.location.protocol) ? "https://example.org/piwik/" : "http://example.org/piwik/");
     document.write(unescape("%3Cscript src='" + pkBaseURL + "piwik.js' type='text/javascript'%3E%3C/script%3E"));
@@ -13,4 +13,4 @@ assert 1 == index.getText().count( '''\
     } catch( err ) {}
     </script><noscript><p><img src="http://example.org/piwik/piwik.php?idsite=1" style="border:0" alt=""/></p></noscript>
     <!-- End Piwik Tag -->
-''' )
+'''.replace( "\n", System.getProperty( "line.separator" ) ) )
