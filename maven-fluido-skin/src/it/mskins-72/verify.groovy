c69e69f96773fbe10768cf7a7fdebe5861f428c0
Olivier Lamy <olamy@apache.org>
2013-11-29 05:06:46 +0000

    make it test more simple
    git-svn-id: https://svn.apache.org/repos/asf/maven/skins/trunk@1546509 13f79535-47bb-0310-9956-ffa450edef68

--- maven-fluido-skin/src/it/mskins-72/verify.groovy
+++ maven-fluido-skin/src/it/mskins-72/verify.groovy
@@ -2,10 +2,4 @@ File index = new File( basedir, "target/site/index.html" )
 int inceptionYear = 2002
 int currentYear = Calendar.getInstance().get(Calendar.YEAR)
 assert index.exists()
-assert 1 == index.getText().count( """\
-                                        <p class="pull-right">Copyright &copy;                    ${inceptionYear}&#x2013;${currentYear}
-                        <a href="http://www.apache.org/">The Apache Software Foundation</a>.
-            All rights reserved.      
-                    
-      </p>
-""".replace( "\n", System.getProperty( "line.separator" ) ) )
+assert 1 == index.getText().count( "<p class=\"pull-right\">Copyright &copy;" )
