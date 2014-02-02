aa2e10a6c5266eb3ead7e91b80b6f9687c01e40a
Tony Chemit <tchemit@apache.org>
2014-02-02 23:26:39 +0000

    MSKINS-91 - Should use as possible https resources
    git-svn-id: https://svn.apache.org/repos/asf/maven/skins/trunk/maven-fluido-skin@1563746 13f79535-47bb-0310-9956-ffa450edef68

--- src/it/mskins-76/verify.groovy
+++ src/it/mskins-76/verify.groovy
@@ -7,12 +7,12 @@ assert 1 == index.getText().count( '''\
             var s = document.createElement('script'), t = document.getElementsByTagName('script')[0];
             s.type = 'text/javascript';
             s.async = true;
-            s.src = 'http://api.flattr.com/js/0.6/load.js?mode=auto';
+            s.src = 'https://api.flattr.com/js/0.6/load.js?mode=auto';
             t.parentNode.insertBefore(s, t);
         })();
     /* ]]> */</script>'''.replace( "\n", System.getProperty( "line.separator" ) ) )
 
 assert 1 == index.getText().count( '''\
 <a class="FlattrButton" style="display:none;"  href="http://opensource.org/"></a>
-    <noscript><a href="http://flattr.com/thing/448050/Open-Source-Initiative-OSI" target="_blank">
-    <img src="http://api.flattr.com/button/flattr-badge-large.png" alt="Flattr this" title="Flattr this" border="0" /></a></noscript>'''.replace( "\n", System.getProperty( "line.separator" ) ) )
+    <noscript><a href="https://flattr.com/thing/448050/Open-Source-Initiative-OSI" target="_blank">
+    <img src="https://api.flattr.com/button/flattr-badge-large.png" alt="Flattr this" title="Flattr this" border="0" /></a></noscript>'''.replace( "\n", System.getProperty( "line.separator" ) ) )
